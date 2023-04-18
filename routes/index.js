7bc15e351dee147b7695c9ded1bd8a41dde745ef
Erykdev06 <lamvip06192003@gmail.com>
2023-04-18 20:15:11 +0700

    add

--- routes/index.js
+++ routes/index.js
@@ -8,49 +8,49 @@ const auth = require('../middle/Authen');
 // http://localhost:3000
 // Hiển thị trang trang chủ
 router.get('/', [auth.authenWeb], async (req, res, next) => {
-	res.render('index');
+  res.render('index');
 });
 
 // http://localhost:3000/login
 // Hiển thị trang login
 router.get('/login', [auth.authenWeb], async (req, res, next) => {
-	res.render('user/login');
+  res.render('user/login');
 });
 
 // http://localhost:3000/logout
 router.get('/logout', [auth.authenWeb], async (req, res, next) => {
-	req.session.destroy();
-	res.redirect('/login');
+  req.session.destroy();
+  res.redirect('/login');
 });
 
 router.get('/product', [auth.authenWeb], async (req, res, next) => {
-	try {
-		const products = await productController.getAllProducts();
-		res.render('product/list', { products });
-	} catch (error) {
-		next(error);
-	}
+  try {
+    const products = await productController.getAllProducts();
+    res.render('product/list', { products });
+  } catch (error) {
+    next(error);
+  }
 });
 
 // http://localhost:3000/login
 // Xử lý trang login
 // Kiểm tra email, password, thành công chuyển sang trang chủ, ngược lại chuyển sang trang login
 router.post('/login', [auth.authenWeb], async (req, res, next) => {
-	try {
-		const { email, password } = req.body;
-		const result = await userController.login(email, password);
-		// Lưu thông tin vào session
-		if (result) {
-			const token = jwt.sign({ _id: result._id }, 'secret');
-			req.session.token = token;
-			return res.redirect('/');
-		} else {
-			return res.redirect('/login');
-		}
-	} catch (error) {
-		console.log('Login error: ', error);
-		return res.redirect('/login');
-	}
+  try {
+    const { email, password } = req.body;
+    const result = await userController.login(email, password);
+    // Lưu thông tin vào session
+    if (result) {
+      const token = jwt.sign({ _id: result._id }, 'secret');
+      req.session.token = token;
+      return res.redirect('/');
+    } else {
+      return res.redirect('/login');
+    }
+  } catch (error) {
+    console.log('Login error: ', error);
+    return res.redirect('/login');
+  }
 });
 
 module.exports = router;
